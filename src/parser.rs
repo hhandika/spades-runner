c067e7371b46aca2532ba8c5011ea875f0382a11
Heru Handika <herubiolog@gmail.com>
2021-03-06 17:11:08 -0600

    Change pathbuf to string. Add more tests.

--- src/parser.rs
+++ src/parser.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use std::path::PathBuf;
 
 use crate::utils;
 
@@ -37,37 +36,43 @@ pub fn parse_seqdir(input: &str) -> Vec<SeqDirs> {
 
 
 pub struct SeqDirs {
-    pub id: PathBuf,
-    pub dir: PathBuf,
+    pub id: String,
+    pub dir: String,
 }
 
 impl SeqDirs {
     fn new() -> Self {
         Self {
-            id: PathBuf::new(),
-            dir: PathBuf::new(),
+            id: String::new(),
+            dir: String::new(),
         }
     }
 
     fn parse_csv(&mut self, line: &str) {
         let sep = ',';
         let lines = utils::split_strings(line, sep);
-        assert_eq!(2, lines.len());
+        self.check_results(&lines);
         self.parse_samples(&lines);
     }
 
     fn parse_ini(&mut self, line: &str) {
         let sep = ':';
         let lines = utils::split_strings(line, sep);
-        assert_eq!(2, lines.len());
+        self.check_results(&lines);
         self.parse_samples(&lines);
     }
 
     fn parse_samples(&mut self, lines: &[String]) {
-        self.id = PathBuf::from(&lines[0]);
-        self.dir = PathBuf::from(&lines[1]);
+        self.id = String::from(&lines[0]);
+        self.dir = String::from(&lines[1]);
     }
 
+    fn check_results(&self, lines: &[String]) {
+        if lines.len() != 2 {
+            panic!("INVALID INPUT. EXPECTING ID AND DIRECTORY PATH, \
+                FOUND: {:?}", lines)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -80,5 +85,44 @@ mod test {
         let samples = parse_seqdir(&input);
 
         assert_eq!(2, samples.len());
+    }
+
+    #[test]
+    fn split_csv_test() {
+        let line = "some_animals,folder/target/";
+        let mut samples = SeqDirs::new();
+
+        samples.parse_csv(&line);
+        assert_eq!("some_animals", samples.id);
+        assert_eq!("folder/target/", samples.dir);
+    }
+
+    #[test]
+    fn split_csv_whitespaces_test() {
+        let line = " some_animals,folder/target/ ";
+        let mut samples = SeqDirs::new();
+
+        samples.parse_csv(&line);
+        assert_eq!("some_animals", samples.id);
+        assert_eq!("folder/target/", samples.dir);
+    }
+
+    #[test]
+    fn split_ini_test() {
+        let line = "some_animals:folder/target/";
+        let mut samples = SeqDirs::new();
+
+        samples.parse_ini(&line);
+        assert_eq!("some_animals", samples.id);
+        assert_eq!("folder/target/", samples.dir);
+    }
+
+    #[test]
+    #[should_panic]
+    fn split_ini_panic_test() {
+        let line = "some_animals:folder/target/:random";
+        let mut samples = SeqDirs::new();
+
+        samples.parse_ini(&line);
     }
 }
