cb20d03ec16d83631f8ab4326b9eb15fb06a9bc8
Heru Handika <herubiolog@gmail.com>
2021-03-06 19:58:50 -0600

    Add system information.

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::path::Path;
 use std::io::{self, Result, Write};
 
+use sysinfo::SystemExt;
+
 pub fn check_dir_exists(dir: &Path) {
     if dir.exists() {
         panic!("{:?} DIR EXISTS. PLEASE RENAME OR REMOVE IT", dir);
@@ -23,4 +25,29 @@ pub fn split_strings(lines: &str, sep: char) -> Vec<String> {
     lines.split(sep)
         .map(|e| e.trim().to_string())
         .collect()
+}
+
+pub fn get_system_info() -> Result<()> {
+    let mut sysinfo = sysinfo::System::new_all();
+    sysinfo.refresh_all();
+
+    let io = io::stdout();
+    let mut handle = io::BufWriter::new(io);
+
+    let total_ram = sysinfo.get_total_memory();
+    let gb = 1048576;
+
+    writeln!(handle, "System Information:")?;
+    
+    writeln!(handle, "Operating System\t: {} {}", 
+        sysinfo.get_name().as_ref().unwrap(),
+        sysinfo.get_os_version().as_ref().unwrap())?;
+
+    writeln!(handle, "Kernel version\t\t: {}", sysinfo.get_kernel_version().as_ref().unwrap())?;
+    writeln!(handle, "Available cores\t\t: {:?}", num_cpus::get_physical())?;
+    writeln!(handle, "Available threads\t: {:?}", num_cpus::get())?;
+    writeln!(handle, "Total RAM\t\t: {} Gb", total_ram/gb)?;
+    writeln!(handle)?;
+
+    Ok(())
 }
