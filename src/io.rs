71079fbfe2f1eeecb07d273f73e714e97f8ac5b0
Heru Handika <herubiolog@gmail.com>
2021-03-08 11:27:08 -0600

    Change origin to dir.

--- src/io.rs
+++ src/io.rs
@@ -44,7 +44,7 @@ fn print_dryrun(dirs: &[SeqReads]) -> Result<()> {
     dirs.iter()
         .for_each(|e| {
             writeln!(handle,"\x1b[0;32mID\t\t: {}\x1b[0m", e.id.to_string_lossy()).unwrap();
-            writeln!(handle,"Origin\t\t: {}", e.dir.to_string_lossy()).unwrap();
+            writeln!(handle,"Dir\t\t: {}", e.dir.to_string_lossy()).unwrap();
             writeln!(handle,"Read 1\t\t: {}", e.read_1.to_string_lossy()).unwrap();
             writeln!(handle,"Read 2\t\t: {}", e.read_2.to_string_lossy()).unwrap();
 
